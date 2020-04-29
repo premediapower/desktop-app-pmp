a2921a0d645923e27847696d5e57d617c64d4eef
Fridzema <fridzema@gmail.com>
2020-04-29 10:32:31 +0200

    Version update

--- src/background.js
+++ src/background.js
@@ -2,12 +2,19 @@
 import path from "path";
 
 
-const { autoUpdater } = require("electron-updater");
+const {
+	autoUpdater
+} = require("electron-updater");
 
-import { app, protocol, BrowserWindow, globalShortcut } from "electron";
+import {
+	app,
+	protocol,
+	BrowserWindow,
+	globalShortcut
+} from "electron";
 import {
 	createProtocol,
-  installVueDevtools
+	installVueDevtools
 } from "vue-cli-plugin-electron-builder/lib";
 
 const isDevelopment = process.env.NODE_ENV !== "production";
@@ -17,88 +24,86 @@ const isDevelopment = process.env.NODE_ENV !== "production";
 let win;
 
 // Scheme must be registered before the app is ready
-protocol.registerSchemesAsPrivileged([
-	{
-    scheme: "app",
-    privileges: {
-      secure: true,
-      standard: true
-    }
-  }
-]);
+protocol.registerSchemesAsPrivileged([{
+	scheme: "app",
+	privileges: {
+		secure: true,
+		standard: true
+	}
+}]);
 
 function createWindow() {
 	// Create the browser window.
-  win = new BrowserWindow({
+	win = new BrowserWindow({
 		show: false,
-    title: "Premediapower desktop app",
-    width: 600,
-    height: 600,
-    useContentSize: true,
-    center: true,
-    frame: false,
-    webgl: false,
-    // alwaysOnTop: true,
-    titleBarStyle: "hidden",
-    transparent: true,
-    darkTheme: true,
-    autoHideMenuBar: false,
-    icon: path.join(__static, "icon.png"),
-    webPreferences: {
+		title: "Premediapower desktop app",
+		width: 600,
+		height: 600,
+		useContentSize: true,
+		center: true,
+		frame: false,
+		webgl: false,
+		// alwaysOnTop: true,
+		titleBarStyle: "hidden",
+		transparent: true,
+		darkTheme: true,
+		autoHideMenuBar: false,
+		icon: path.join(__static, "icon.png"),
+		webPreferences: {
 			nodeIntegration: true
-    }
-  });
-	
-  win.maximizable = false;
-  win.fullScreenable = false;
-	
-  if (process.env.WEBPACK_DEV_SERVER_URL) {
+		}
+	});
+
+	win.maximizable = false;
+	win.fullScreenable = false;
+
+	if (process.env.WEBPACK_DEV_SERVER_URL) {
 		// Load the url of the dev server if in development mode
-    win.loadURL(process.env.WEBPACK_DEV_SERVER_URL);
-    if (!process.env.IS_TEST) win.webContents.openDevTools();
-  } else {
-    createProtocol("app");
-    // Load the index.html when not in development
-    win.loadURL("app://./index.html");
-    autoUpdater.checkForUpdatesAndNotify();
-  }
+		win.loadURL(process.env.WEBPACK_DEV_SERVER_URL);
+		if (!process.env.IS_TEST) win.webContents.openDevTools();
+	} else {
+		createProtocol("app");
+		// Load the index.html when not in development
+		win.loadURL("app://./index.html");
+		autoUpdater.checkForUpdatesAndNotify();
+	}
 
-  win.on("will-quit", () => {
+	win.on("will-quit", () => {
 		// Unregister all shortcuts.
-    globalShortcut.unregisterAll();
-  });
+		globalShortcut.unregisterAll();
+	});
 
-  win.on("closed", () => {
-    win = null;
-  });
-	
-  win.on("ready-to-show", () => {
+	win.on("closed", () => {
+		win = null;
+	});
+
+	win.on("ready-to-show", () => {
 		win.show();
-  });
+	});
 }
 
 // Quit when all windows are closed.
 app.on("window-all-closed", () => {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== "darwin") {
+	// On macOS it is common for applications and their menu bar
+	// to stay active until the user quits explicitly with Cmd + Q
+	if (process.platform !== "darwin") {
 		app.quit();
-  }
+	}
 });
 
 app.on("activate", () => {
 	// On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (win === null) {
+	// dock icon is clicked and there are no other windows open.
+	if (win === null) {
 		createWindow();
-  }
+	}
 });
 
 app.on("ready", () => {
 	// Register a 'CommandOrControl+X' shortcut listener.
-  const ret = globalShortcut.register("CommandOrControl+P", () => {
-		win.show();
-  });
+	// const ret = globalShortcut.register("CommandOrControl+P", () => {
+	// 	win.show();
+	// });
 });
 
 // This method will be called when Electron has finished
@@ -107,34 +112,34 @@ app.on("ready", () => {
 app.on("ready", async () => {
 	if (isDevelopment && !process.env.IS_TEST) {
 		// Install Vue Devtools
-    // Devtools extensions are broken in Electron 6.0.0 and greater
-    // See https://github.com/nklayman/vue-cli-plugin-electron-builder/issues/378 for more info
-    // Electron will not launch with Devtools extensions installed on Windows 10 with dark mode
-    // If you are not using Windows 10 dark mode, you may uncomment these lines
-    // In addition, if the linked issue is closed, you can upgrade electron and uncomment these lines
-    try {
+		// Devtools extensions are broken in Electron 6.0.0 and greater
+		// See https://github.com/nklayman/vue-cli-plugin-electron-builder/issues/378 for more info
+		// Electron will not launch with Devtools extensions installed on Windows 10 with dark mode
+		// If you are not using Windows 10 dark mode, you may uncomment these lines
+		// In addition, if the linked issue is closed, you can upgrade electron and uncomment these lines
+		try {
 			await installVueDevtools();
-    } catch (e) {
-      console.error("Vue Devtools failed to install:", e.toString());
-    }
-  }
-	
-  createWindow();
+		} catch (e) {
+			console.error("Vue Devtools failed to install:", e.toString());
+		}
+	}
+
+	createWindow();
 });
 
 // Exit cleanly on request from parent process in development mode.
 if (isDevelopment) {
 	if (process.platform === "win32") {
 		process.on("message", data => {
-      if (data === "graceful-exit") {
+			if (data === "graceful-exit") {
 				app.quit();
-      }
-    });
-  } else {
-    process.on("SIGTERM", () => {
+			}
+		});
+	} else {
+		process.on("SIGTERM", () => {
 			app.quit();
-    });
-  }
+		});
+	}
 }
 
 require("v8-compile-cache");
--- src/main.js
+++ src/main.js
@@ -9,60 +9,60 @@ window._ = require("lodash");
 
 const appVersion = require("../package.json").version;
 
-axios.defaults.baseURL = "https://dev.premediapower.com";
+axios.defaults.baseURL = "https://premediapower.com";
 axios.defaults.headers.common["Content-Type"] = "application/json";
 axios.defaults.headers.common["Accept"] = "application/json";
 // https: //github.com/laravel/airlock
 axios.defaults.withCredentials = true;
 
 axios.interceptors.request.use(
-  function(config) {
-    const token = window.localStorage.getItem("apiToken");
-    if (token != null) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-  },
-  function(error) {
-    return Promise.reject(error.response);
-  }
+	function (config) {
+		const token = window.localStorage.getItem("apiToken");
+		if (token != null) {
+			config.headers.Authorization = `Bearer ${token}`;
+		}
+		return config;
+	},
+	function (error) {
+		return Promise.reject(error.response);
+	}
 );
 
 Vue.config.productionTip = false;
 Vue.config.preserveWhitespace = false;
 
 Object.defineProperty(Vue.prototype, "$_", {
-  value: _
+	value: _
 });
 
 var app = {
-  version: appVersion,
-  debug: process.env.NODE_ENV !== "production",
-  connected_to_internet: true
+	version: appVersion,
+	debug: process.env.NODE_ENV !== "production",
+	connected_to_internet: true
 };
 
 new Vue({
-  data: app,
-  mounted() {
-    const vm = this;
-    console.log("Vue Instance mounted");
+	data: app,
+	mounted() {
+		const vm = this;
+		console.log("Vue Instance mounted");
 
-    window.addEventListener(
-      "online",
-      function() {
-        vm.connected_to_internet = true;
-      },
-      true
-    );
-    window.addEventListener(
-      "offline",
-      function() {
-        vm.connected_to_internet = false;
-      },
-      true
-    );
-  },
-  render: function(h) {
-    return h(App);
-  }
+		window.addEventListener(
+			"online",
+			function () {
+				vm.connected_to_internet = true;
+			},
+			true
+		);
+		window.addEventListener(
+			"offline",
+			function () {
+				vm.connected_to_internet = false;
+			},
+			true
+		);
+	},
+	render: function (h) {
+		return h(App);
+	}
 }).$mount("#app");
